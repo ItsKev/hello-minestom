071608289dc5afdf92a270c630092ad175978e95
ItsKev <kevin.huber@hotmail.com>
2025-12-13 19:12:43 +0100

    Extend the entity ai with a simple stroll goal

--- src/main/java/org/example/entitites/CustomZombie.java
+++ src/main/java/org/example/entitites/CustomZombie.java
@@ -4,14 +4,20 @@ import net.minestom.server.entity.EntityCreature;
 import net.minestom.server.entity.EntityType;
 import net.minestom.server.entity.ai.EntityAIGroupBuilder;
 import net.minestom.server.entity.ai.goal.RandomLookAroundGoal;
+import net.minestom.server.entity.ai.goal.RandomStrollGoal;
+import net.minestom.server.entity.attribute.Attribute;
 
 public class CustomZombie extends EntityCreature {
 
     public CustomZombie() {
         super(EntityType.ZOMBIE);
+
+        getAttribute(Attribute.MOVEMENT_SPEED).setBaseValue(0.1);
+
         addAIGroup(
                 new EntityAIGroupBuilder()
                         .addGoalSelector(new RandomLookAroundGoal(this, 20))
+                        .addGoalSelector(new RandomStrollGoal(this, 6))
                         .build()
         );
     }
